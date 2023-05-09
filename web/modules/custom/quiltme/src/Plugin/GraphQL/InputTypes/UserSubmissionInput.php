beeda5cbcb0f0708d96db13c7a905ec405a5e5b8
Mike Goulding <mike.goulding@fourkitchens.com>
2023-05-09 10:46:20 -0500

    Starting mutation work

---
+++ web/modules/custom/quiltme/src/Plugin/GraphQL/InputTypes/UserSubmissionInput.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\quiltme\Plugin\GraphQL\InputTypes;
+
+use Drupal\graphql\Plugin\GraphQL\InputTypes\InputTypePluginBase;
+
+/**
+ * @GraphQLInputType(
+ *   id = "user_submission_input",
+ *   name = "UserSubmissionInput",
+ *   fields = {
+ *     "email" = "String",
+ *     "image" = "String",
+ *     "backgroundImage" = "String"
+ *   }
+ * )
+ */
+class UserSubmissionInput extends InputTypePluginBase {
+}
---
+++ web/modules/custom/quiltme/src/Plugin/GraphQL/Mutations/UserSubmissionMutation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\quiltme\Plugin\GraphQL\Mutations;
+
+use Drupal\graphql\GraphQL\Execution\ResolveContext;
+use Drupal\graphql_core\Plugin\GraphQL\Mutations\Entity\CreateEntityBase;
+use GraphQL\Type\Definition\ResolveInfo;
+
+/**
+ * Simple mutation for creating a new article node.
+ *
+ * @GraphQLMutation(
+ *   id = "create_user_submission",
+ *   entity_type = "node",
+ *   entity_bundle = "user_submission",
+ *   secure = true,
+ *   name = "createUserSubmission",
+ *   type = "EntityCrudOutput!",
+ *   arguments = {
+ *     "input" = "UserSubmissionInput"
+ *   }
+ * )
+ */
+class UserSubmissionMutation extends CreateEntityBase {
+
+  protected function extractEntityInput($value, array $args, ResolveContext $context, ResolveInfo $info) {
+    return [
+      'field_user_email' => $args['input']['email'],
+      'field_original_image' => $args['input']['image'],
+      'field_background_image' => $args['input']['backgroundImage'],
+    ];
+  }
+}
